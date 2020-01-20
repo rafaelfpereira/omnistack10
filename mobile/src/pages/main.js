4fa2f23623d4a556c6a41460e3d3d7b63a34a748
Rafael Pereira <rafael.freitasiv@gmail.com>
2020-01-19 23:54:39 -0300

    added styling and search bar

--- mobile/src/pages/main.js
+++ mobile/src/pages/main.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Image, View, Text } from 'react-native'
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons'
+
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null)
   useEffect(() => {
@@ -32,21 +41,45 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -29.7451705, longitude: -51.0878812 }}>
-        <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/4705113?s=460&v=4' }}/>
-        
-        <Callout onPress={() => {
-          navigation.navigate('Profile', { github_username: 'rafaelfpereira' })
-        }}>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>O Nome do Vivente</Text>
-            <Text style={styles.devBio}>Como o vivente quer ser reconhecido e pelo quê.</Text>
-            <Text style={styles.devTechs}>React, React e React</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -29.7451705, longitude: -51.0878812 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: 'https://avatars0.githubusercontent.com/u/4705113?s=460&v=4'
+            }}
+          />
+
+          <Callout
+            onPress={() => {
+              navigation.navigate('Profile', {
+                github_username: 'rafaelfpereira'
+              })
+            }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>O Nome do Vivente</Text>
+              <Text style={styles.devBio}>
+                Como o vivente quer ser reconhecido e pelo quê.
+              </Text>
+              <Text style={styles.devTechs}>React, React e React</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Buscar devs por tecnologia...'
+          placeholderTextColor='#999'
+          autoCapitalize='words'
+          autoCorrect={false}
+        />
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+            <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   )
 }
 
@@ -60,26 +93,64 @@ const styles = StyleSheet.create({
     height: 54,
     borderRadius: 4,
     borderWidth: 4,
-    borderColor: '#FFF',
+    borderColor: '#FFF'
   },
 
   callout: {
-    width: 260,
+    width: 260
   },
 
   devName: {
     fontWeight: 'bold',
-    fontSize: 16,
+    fontSize: 16
   },
 
   devBio: {
     color: '#666',
-    marginTop: 5,
+    marginTop: 5
   },
 
   devTechs: {
-    marginTop: 5,
-  }
+    marginTop: 5
+  },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4DFF',
+    color: '#000',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+  },
+
 })
 
 export default Main
